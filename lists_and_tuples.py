a3e3335437bec984ce098f660744f8bb8bbb180c
GAURAV PATHAK <140134922+coderzgaurav01@users.noreply.github.com>
2025-05-01 22:37:19 +0530

    Lists and Tuples

---
+++ lists_and_tuples.py
@@ -0,0 +1,33 @@
+# List is a built-in data type that store set of values
+marks=[94.4,56.6,95.2,66.4]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+
+#list store value of various datatype
+student=["Ajay",99,20,"Giridih"]
+print(student)
+
+#List slicing
+print(marks[1:4])
+print(marks[-3:-1])
+
+#List methods
+    #append() it's append at the end of list
+marks.append(99.2)
+print(marks)
+    #sort() it sort the list
+marks.sort()
+print(marks)
+
+#reverse() it reverse the list
+marks.sort(reverse=True)
+print(marks)
+
+marks.reverse()
+print(marks)
+
+#insert() it add elemnt at particular position
+marks.insert(3,78.8)
+print(marks)
