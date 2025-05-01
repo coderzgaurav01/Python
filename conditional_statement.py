24d6dd8a08315000c07107929c72614c3fb33a6c
GAURAV PATHAK <140134922+coderzgaurav01@users.noreply.github.com>
2025-05-01 19:03:00 +0530

    Conditional statement

--- conditional_statement.py
+++ conditional_statement.py
@@ -1,3 +1,4 @@
+"""
 age = int(input("Enter Your age: "))
 if(age>18):
     print("You are eligible to drive") 
@@ -5,3 +6,18 @@ elif(age>=16 and age<18):
     print("Wait for 2 year bro ")  
 else:
     print("You are not eligible bro!!")
+"""
+    
+#Grading system
+name = input("Enter Your Name : ")
+print("Welcome in the world of education Mr.",name)
+print("You are here to know your grade based on your marks")
+marks = int(input("Enter the marks :"))
+if(marks>=90):
+    print("Grade A")
+elif(marks<90 and marks>=80):
+    print("Grade B")
+elif(marks<80 and marks>=70):
+    print("Grade C")
+else:
+    print("Grade D")
