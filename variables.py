b33182e664df2999885031f49674124eaf8672bd
GAURAV PATHAK <140134922+coderzgaurav01@users.noreply.github.com>
2025-05-01 15:53:05 +0530

    ariable syntax and rule

--- variables.py
+++ variables.py
@@ -2,3 +2,9 @@ name='Gaurav'
 age=23
 price=800000
 print(name,age,price)
+
+#Rules for declaring variable
+    # variable name cannot be start with any digit
+    # special symbols can't be used
+    # variable can be of any length
+    # variables can be the combination of Uppercase letter, lowercase letter , digit and underscore
