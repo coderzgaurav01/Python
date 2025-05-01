213499fa13fa7f411801b7a668ae65fa96e75e86
GAURAV PATHAK <140134922+coderzgaurav01@users.noreply.github.com>
2025-05-01 16:28:47 +0530

    arithmetical operator

---
+++ operators.py
@@ -0,0 +1,18 @@
+# An operator is a symbol that performs a certain operation between operands
+
+'''
+    > Arithmetic Operators(+,-,*,/,%,**)
+    > Relational/Comparison Operators(==,!=,>,<,>=,<=)
+    > Assignment Operators(=,+=,-=,*=,/=,%=,**=)
+    > Logical Operators(not,and,or)
+
+
+'''
+a=5
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
