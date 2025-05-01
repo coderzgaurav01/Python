fe7dad990dc34015a301a3643437132724566483
GAURAV PATHAK <140134922+coderzgaurav01@users.noreply.github.com>
2025-05-01 16:08:26 +0530

    Data types in Python

---
+++ Data_types.py
@@ -0,0 +1,17 @@
+""" Data Types
+    #Integers
+    # String
+    # Float
+    # Boolean 
+    # None 
+"""
+Name= "Gaurav"
+Marks=99.9
+salary=1000000  
+old = False
+a=None
+print(Name,type(Name))
+print(Marks, type(Marks))
+print(salary,type(salary))
+print(old,type(old))
+print(a,type(a))
