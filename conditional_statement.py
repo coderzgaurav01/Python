44598f339023cc5460536d6914ac9618ae955f7c
GAURAV PATHAK <140134922+coderzgaurav01@users.noreply.github.com>
2025-05-01 18:55:34 +0530

    Conditional statement

---
+++ conditional_statement.py
@@ -0,0 +1,7 @@
+age = int(input("Enter Your age: "))
+if(age>18):
+    print("You are eligible to drive") 
+elif(age>=16 and age<18):
+    print("Wait for 2 year bro ")  
+else:
+    print("You are not eligible bro!!")
